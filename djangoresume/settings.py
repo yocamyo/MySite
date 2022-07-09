c9a4c95eee7362ff0c468132af5695b249b94de2
Camden Navis <naviscam@gmail.com>
2022-07-09 17:42:44 -0400

    created blog app and Post model. Edited admin page view.

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    "blog.apps.BlogConfig",
     "resume.apps.ResumeConfig",
 ]
 
