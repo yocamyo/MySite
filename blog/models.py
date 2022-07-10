d2a31bf8a9727f3cc29116a51a6522f30638446c
Camden Navis <naviscam@gmail.com>
2022-07-10 17:58:06 -0400

    added pagination and post detail view

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status="published")
 
 
 class Post(models.Model):
@@ -25,5 +31,11 @@ class Post(models.Model):
     class Meta:
         ordering = ("-publish",)
 
-    def __str__(self):
-        return self.title
+        def __str__(self):
+            return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.core.paginator import Paginator, PageNotAnInteger
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.published.all()
+
+    paginator = Paginator(posts, 10)  # 10 posts in each page
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, "post_list.html", {"posts": posts, page: "pages"})
+
+
+def post_detail(request, post):
+    post = get_object_or_404(Post, slug=post, status="published")
+    return render(request, "post_detail.html", {"post": post})
