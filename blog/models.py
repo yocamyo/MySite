a3c2789726723c907fe7cd0ed8b43f5cfe2a0872
Camden Navis <naviscam@gmail.com>
2022-07-10 21:53:03 -0400

    Added comments functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from tkinter.tix import Tree
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -41,3 +42,27 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("blog:post_detail", args=[self.slug])
+
+    def get_comments(self):
+        return self.comments.filter(parent=None).filter(active=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    parent = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
+    body = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ("created",)
+
+    def __str__(self):
+        return self.body
+
+    def get_comments(self):
+        return Comment.objects.filter(parent=self).filter(active=True)
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.core.paginator import Paginator, PageNotAnInteger
+from .forms import CommentForm
 from .models import Post
 
 
@@ -22,4 +23,49 @@ def post_list(request):
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug=post, status="published")
-    return render(request, "post_detail.html", {"post": post})
+
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+            # redirect to same page and focus on that comment
+            return redirect(post.get_absolute_url() + "#" + str(new_comment.id))
+        else:
+            comment_form = CommentForm()
+
+    return render(
+        request,
+        "post_detail.html",
+        {"post": post, "comments": comments, "comment_form": comment_form},
+    )
+
+
+def reply_page(request):
+    if request.method == "POST":
+
+        form = CommentForm(request.Post)
+
+        if form.is_valid():
+            post_id = request.POST.get("post_id")
+            parent_id = request.POST.get("parent")
+            post_url = request.POST.get("post_url")
+
+            reply = form.save(commit=False)
+
+            reply.post = Post(id=post_id)
+            reply.parent = Comment(id=parent_id)
+            reply.save()
+
+            return redirect(post_url + "#" + str(reply.id))
+
+    return redirect("/")
